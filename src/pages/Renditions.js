f38582ca4bacf68429a675f99cb36ead5aca46a5
calmfors <erikcalmfors@outlook.com>
2020-09-14 20:28:12 +0200

    Caption works

--- src/pages/Renditions.js
+++ src/pages/Renditions.js
@@ -8,49 +8,30 @@ const Renditions = ({ match }) => {
   const [doc, setDocData] = useState(null);
   const [notFound, toggleNotFound] = useState(false);
 
-  console.log(match);
-
   const uid = match.params.uid;
 
-  // console.log('match:')
-  // console.log(tag)
-
-  // Get the page document from Prismic
   useEffect(() => {
     const fetchData = async () => {
-      // We are using the function to get a document by its UID
-      // const result = await client.getByUID('blog', uid)
 
       //Get list of all the categories from prismic
       const categories = await client.query(
         Prismic.Predicates.at("document.type", "work")
       );
 
-      console.log(categories);
-      console.log(uid);
-
-      //Match the url work category with the list of categories and find correct id
+      //Match the url work category with the list of rendition and find correct id
       const category_id = categories.results.filter(item => item.slugs[0] === uid)[0].id;
 
-      console.log(category_id);
-
-      //Get posts with the choosen category, with the id
+      //Get renditons with the choosen category, with the id
       const result = await client.query(
         Prismic.Predicates.at("my.rendition.work_category", category_id)
       );
 
-      // const result = await client.query(
-      //   Prismic.Predicates.at("document.type", "rendition")
-      // );
-
-
-      console.log(result);
-
       if (result) {
+
         // We use the State hook to save the document
-        console.log(result);
         return setDocData(result);
       } else {
+
         // Otherwise show an error message
         console.warn(
           "Blog document not found. Make sure it exists in your Prismic repository"
@@ -62,26 +43,23 @@ const Renditions = ({ match }) => {
   }, [uid]); // Skip the Effect hook if the UID hasn't changed
 
   if (doc) {
-    console.log("doc")
-    console.log(doc)
+
     return doc.results.map((item, i) => (
       <div key={i} className="renditions">
         {/* This is how to render a Rich Text field as plain text*/}
         <h1 key={"a" + i}>{RichText.asText(item.data.rendition_title)}</h1>
-        {/* This is how to render a Rich Text field into your template as HTML */}
-        <RichText
-          key={"b" + i}
-          render={item.data.caption}
-          linkResolver={linkResolver}
-        />
-        {/* This is how to get an image into your template */}
-        {item.data.rendition_images.map((image, i) => (
+
+        {item.data.rendition_images.map((image, i) => ([
           <img
             src={image.rendition_image.url}
             key={"c" + i}
-
-          />
-
+            alt={image.rendition_image_caption[0].text}
+          />,
+          <RichText
+            key={"b" + i}
+            render={image.rendition_image_caption}
+            linkResolver={linkResolver}
+          />]
         ))}
 
       </div>
