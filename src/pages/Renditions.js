624455f7e482e4e4722ed9d5a5037918a631823d
calmfors <erikcalmfors@outlook.com>
2020-09-15 12:51:40 +0200

    Added work_title and script in renditions

--- src/pages/Renditions.js
+++ src/pages/Renditions.js
@@ -18,16 +18,18 @@ const Renditions = ({ match }) => {
         Prismic.Predicates.at("document.type", "work")
       );
 
+
       //Match the url work category with the list of rendition and find correct id
-      const category_id = categories.results.filter(item => item.slugs[0] === uid)[0].id;
+      const category = categories.results.filter(item => item.slugs[0] === uid)[0];
 
       //Get renditons with the choosen category, with the id
       const result = await client.query(
-        Prismic.Predicates.at("my.rendition.work_category", category_id)
+        Prismic.Predicates.at("my.rendition.work_category", category.id)
       );
 
       if (result) {
-
+        result.work_script = category.data.work_script
+        result.work_title = category.data.work_title
         // We use the State hook to save the document
         return setDocData(result);
       } else {
@@ -43,8 +45,19 @@ const Renditions = ({ match }) => {
   }, [uid]); // Skip the Effect hook if the UID hasn't changed
 
   if (doc) {
-
-    return doc.results.map((item, i) => (
+    return [<div key="a">
+      <RichText
+        key="b"
+        render={doc.work_title}
+        linkResolver={linkResolver}
+      />
+      <RichText
+        key="c"
+        render={doc.work_script}
+        linkResolver={linkResolver}
+      />
+    </div>,
+    doc.results.map((item, i) => (
       <div key={i} className="renditions">
         {/* This is how to render a Rich Text field as plain text*/}
         <h1 key={"a" + i}>{RichText.asText(item.data.rendition_title)}</h1>
@@ -52,18 +65,18 @@ const Renditions = ({ match }) => {
         {item.data.rendition_images.map((image, i) => ([
           <img
             src={image.rendition_image.url}
-            key={"c" + i}
+            key={"b" + i}
             alt={image.rendition_image_caption[0].text}
           />,
           <RichText
-            key={"b" + i}
+            key={"c" + i}
             render={image.rendition_image_caption}
             linkResolver={linkResolver}
           />]
         ))}
 
       </div>
-    ));
+    ))];
   } else if (notFound) {
     return <NotFound />;
   }
