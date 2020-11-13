bde661e6ceec7db9a7156bb0040d5ad84f888f38
calmfors <erikcalmfors@outlook.com>
2020-11-13 16:25:02 +0100

    New hover-effect

---
+++ build/precache-manifest.841b18da6c3cfd674f57ec11c1d6ba1a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb3b679b56d6df0ee66d7100ff7201da",
+    "url": "/tris/index.html"
+  },
+  {
+    "revision": "294ef2e3a7095e131b3a",
+    "url": "/tris/static/css/main.9e675cd2.chunk.css"
+  },
+  {
+    "revision": "0bf484532521474a135f",
+    "url": "/tris/static/js/2.1c372b97.chunk.js"
+  },
+  {
+    "revision": "f084096847a95456352bd8eec0a72549",
+    "url": "/tris/static/js/2.1c372b97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "294ef2e3a7095e131b3a",
+    "url": "/tris/static/js/main.72a1ad54.chunk.js"
+  },
+  {
+    "revision": "54aaed1a564232fc9686",
+    "url": "/tris/static/js/runtime-main.cfd97a04.js"
+  },
+  {
+    "revision": "f885e5ba2e55b49340bba6bfd370b148",
+    "url": "/tris/static/media/1.f885e5ba.svg"
+  },
+  {
+    "revision": "040ccb6bedd43e139ad82bfd09c99c6f",
+    "url": "/tris/static/media/2.040ccb6b.svg"
+  },
+  {
+    "revision": "d71a10cabc988cb7d910e437279be70a",
+    "url": "/tris/static/media/3.d71a10ca.svg"
+  },
+  {
+    "revision": "fbbaa42698c93f770ef29ffafc3d0cd4",
+    "url": "/tris/static/media/3p.fbbaa426.svg"
+  },
+  {
+    "revision": "810089b9bb7f2cc35b3a1d27063bdb21",
+    "url": "/tris/static/media/4.810089b9.svg"
+  },
+  {
+    "revision": "c350d556f1667b0d77403f6a09dccc8b",
+    "url": "/tris/static/media/5.c350d556.svg"
+  },
+  {
+    "revision": "853d7237930e869786859f09e56921db",
+    "url": "/tris/static/media/6.853d7237.svg"
+  },
+  {
+    "revision": "0c6b8e07701bde6d661902803104aab1",
+    "url": "/tris/static/media/PTMono-Regular.0c6b8e07.ttf"
+  },
+  {
+    "revision": "a97ccf1e30117c053dd28f265c270a22",
+    "url": "/tris/static/media/PTSans-Italic.a97ccf1e.ttf"
+  },
+  {
+    "revision": "4ea26cd5e7f64894d6c2451446f7dda5",
+    "url": "/tris/static/media/PTSans-Regular.4ea26cd5.ttf"
+  }
+]);
